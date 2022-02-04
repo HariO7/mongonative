ac57da41b57d19ce0f469ae7174acc1669a7e843
hari07 <hariodukkathil@gmail.com>
2022-02-04 13:48:36 +0530

    READ

--- index.js
+++ index.js
@@ -1,45 +1,34 @@
 const { MongoClient } = require('mongodb');
 const uri = 'mongodb://127.0.0.1:27017';
-  
-const client = new MongoClient(uri, { 
-    useUnifiedTopology: true, 
-    useNewUrlParser: true 
+
+const client = new MongoClient(uri, {
+	useUnifiedTopology: true,
+	useNewUrlParser: true
 });
-  
+
 connect();
-  
+
 async function connect() {
-    try {
-        await client.connect();
+	try {
+	    await client.connect();
         const db = client.db('cars');
-        console.log(
-    `Successfully connected to db ${db.databaseName}`);
-  
+
         const sportsCars = db.collection('SportsCars');
-      
-        // Insertion
-        const cursorInsertion = await sportsCars.insertMany([
-            {
-                'company': 'mercedes',
-                'series': 'Black Series',
-                'model': 'SLS AMG'
-            },
-            {
-                'company': 'Audi',
-                'series': 'A series',
-                'model': 'A8'
-            }]);
-        console.log(cursorInsertion.insertedCount);
-          
-        // Display
+        const cursorupdate = await sportsCars.updateOne(
+            {company : "mercedes"},
+            {"$set": {"status" : "sold"}}
+        );
+
+        console.log(cursorupdate.modifiedCount);
+        //display
         const cursorFind = sportsCars.find();
         const data = await cursorFind.toArray();
         console.table(data);
-    }
-    catch (err) {
-        console.error(`we encountered ${err}`);
-    }
-    finally {
-        client.close();
-    }
+	}
+	catch (err) {
+		console.error(`we encountered ${err}`);
+	}
+	finally {
+		client.close();
+	}
 }
